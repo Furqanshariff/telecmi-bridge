0bdecb8b5ae1728bd3659d44fb605b1ed13fc5f7
Mohammed Furqan shariff <143084500+Furqanshariff@users.noreply.github.com>
2026-04-26 16:02:00 +0530

    Create call.js

---
+++ api/call.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  const rawPhone = req.body.to || "";
+  const cleanPhone = parseInt(rawPhone.replace(/\D/g, ""));
+  
+  const payload = {
+    user_id: "5003_33337829",
+    secret: "ff60d978-7d3d-431f-96d0-1e4b6a24049a",
+    to: cleanPhone,
+    webrtc: false,
+    followme: true
+  };
+
+  const response = await fetch("https://rest.telecmi.com/v2/webrtc/click2call", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
