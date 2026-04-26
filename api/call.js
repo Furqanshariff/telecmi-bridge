37318efba1a7133a6fd7ea5036d9cec560f9e5d9
Mohammed Furqan shariff <143084500+Furqanshariff@users.noreply.github.com>
2026-04-26 16:28:31 +0530

    Update call.js

--- api/call.js
+++ api/call.js
@@ -1,13 +1,14 @@
 export default async function handler(req, res) {
   const rawPhone = req.body.to || "";
-  const cleanPhone = parseInt(rawPhone.replace(/\D/g, ""));
-  
+  const digits = String(rawPhone).replace(/\D/g, "");
+  const cleanPhone = parseInt(digits.startsWith("91") ? digits : "91" + digits);
+
   const payload = {
     user_id: "5003_33337829",
     secret: "ff60d978-7d3d-431f-96d0-1e4b6a24049a",
     to: cleanPhone,
-    webrtc: true,
-    followme: false
+    webrtc: false,
+    followme: true
   };
 
   const response = await fetch("https://rest.telecmi.com/v2/webrtc/click2call", {
